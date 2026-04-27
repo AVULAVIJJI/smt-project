9675313e347fdd5561c545be5d783b447f01422b
softmastertech <softmastertecch@gmail.com>
2026-04-27 18:30:47 +0530

    om

--- smt-project-updated/smt-frontend/vite.config.js
+++ smt-project-updated/smt-frontend/vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000', // ✅ FIXED
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       }
     }
